80de4b3a2c57818403cd6c3680609ce6d7d22a09
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 14:40:09 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,57 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
-namespace EcuatiaDeGradII
+namespace NDivideK
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            float a, b, c, dt;
-            double x1, x2;
-            Console.Write(" a = ");
-            a = float.Parse(Console.ReadLine());
-            Console.Write(" b = ");
-            b = float.Parse(Console.ReadLine());
-            Console.Write(" c = ");
-            c = float.Parse(Console.ReadLine());
-            if (a == 0)
-                if (b == 0)
-                    if (c == 0)
-                        Console.WriteLine("Ecuatie nedeterminata");
-                    else
-                        Console.WriteLine("Ecuatie imposibila");
-                else
-                {
-                    x1 = -c / b;
-                    Console.WriteLine("Ecuatie de grad 1 cu x1 = {0}", x1);
-                }
+            int n, k;
+            Console.WriteLine("Introduceti n si k:");
+            n = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+            if (k == 0)
+            {
+                Console.WriteLine("Nu se poate imparti la zero!");
+            }
+            else if (n % k == 0)
+            {
+                Console.WriteLine("n se divide cu k");
+            }
             else
             {
-                dt = b * b - 4 * a * c;
-                if (dt < 0)
-                    Console.WriteLine("Ecuatia are solutii complexe");
-                else
-                {
-                    if (dt == 0)
-                    { x1 = x2 = -b / (2 * a);
-                        Console.WriteLine("x1 = x2 = {0}", x1);
-
-                    }
-                    else
-                    {
-                        x1 = (-b + Math.Sqrt(dt)) / 2 * a;
-                        x2 = (-b - Math.Sqrt(dt)) / 2 * a;
-                        Console.WriteLine(" Solutiile sunt x1 = {0}, x2 = {1} ", x1, x2);
-
-                    }
-                }
+                Console.WriteLine("n nu se divide cu k");
             }
-
-            Console.ReadLine();
         }
     }
 }
+
